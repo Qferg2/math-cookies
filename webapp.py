bf9ae868c0a0bf8be8019e2f5d8f1dcffafe8934
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2020-01-28 10:01:13 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -28,19 +28,19 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    #TODO: Subtraction
+    #TODO: save the first and last name in the session
     #session["last_name"] = request.form["lastName"]
     return render_template('page2.html')
   
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    #TODO: Multiplication 
+    #TODO: save the first and last name in the session
     #session["last_name"] = request.form["lastName"]
     return render_template('page3.html')
   
 @app.route('/page4',methods=['GET','POST'])
 def renderPage4():
-    #TODO: Division 
+    #TODO: save the first and last name in the session
     #session["last_name"] = request.form["lastName"]
     return render_template('page4.html')
 
